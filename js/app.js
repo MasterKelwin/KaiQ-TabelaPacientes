39f45b0cb915aa5b9704f1f5bc0ae104ffe4340f
MasterKelwin <mkelwin@outlook.com>
2022-06-27 14:54:36 -0300

    função de calcular imc implementada com loop

--- js/app.js
+++ js/app.js
@@ -29,4 +29,24 @@ function trocaTema() {
 }
 
 
-// FORM
+
+// CALCULA IMC
+
+const pacientes = document.querySelectorAll('.paciente');
+var imcCalculado;
+console.log(pacientes);
+
+function calculaIMC(peso, altura) {
+    return imcCalculado = peso / (altura * altura)
+}
+
+for (i = 0; i < pacientes.length; i++) {
+    const peso = parseFloat((pacientes[i].querySelector('.info-peso')).textContent);
+    const altura = parseFloat((pacientes[i].querySelector('.info-altura')).textContent);
+    var imc = pacientes[i].querySelector('.info-imc');
+
+    calculaIMC(peso, altura);
+    imc.textContent = imcCalculado.toFixed(2);
+    console.log(imc);
+}
+
