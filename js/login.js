c4cf3250e0b928d20a456258d26ec86ce37be7e3
MasterKelwin <mkelwin@outlook.com>
2022-07-02 11:56:54 -0300

    login implementado, senha movimento

---
+++ js/login.js
@@ -0,0 +1,46 @@
+const input = document.querySelector('#password');
+var senha = "movimento";
+const querobel = document.querySelector('.querobel');
+const trace1 = document.querySelector('.trace1');
+const trace2 = document.querySelector('.trace2');
+const trace3 = document.querySelector('.trace3');
+const tag = document.querySelector('.tag');
+
+
+input.focus();
+
+function loga() {
+    if (input.value == senha) {
+        animacaoKaiQ();
+    } else {
+        alert("senha incorreta")
+    }
+}
+
+function animacaoKaiQ () {
+    setTimeout(() => {  
+        querobel.classList.add('querobel-ativo');
+    }, 10);
+
+    setTimeout(() => {  
+        trace1.classList.add('doble-trace-ativo');
+        trace2.classList.add('doble-trace-ativo');
+    }, 1500);
+
+    setTimeout(() => {  
+        trace3.classList.add('trace3-ativo');
+    }, 2500);
+
+    setTimeout(() => {  
+        tag.classList.add('tag-ativo');
+    }, 3500);  
+
+    setTimeout(() => {
+        redireciona();
+    }, 4500);    
+}
+
+function redireciona() {
+    window.location.href = "./calculador de IMC.html";
+}
+
