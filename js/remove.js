28fdb055a53c6b3a1b62e9b70a2fe6b612f848e0
MasterKelwin <mkelwin@outlook.com>
2022-07-05 16:22:05 -0300

    implementação da feature de remoção, agora temos um CRUD completo

---
+++ js/remove.js
@@ -0,0 +1,7 @@
+tabela.addEventListener("dblclick",(event) => {
+    event.target.parentNode.classList.add("fade-out");
+
+    setTimeout(() => {
+        event.target.parentNode.remove();
+    }, 500);    
+});
