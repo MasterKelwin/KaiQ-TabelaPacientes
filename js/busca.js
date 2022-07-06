e5d5170afc62a814351053c6b5f0f8cf2df85d3a
MasterKelwin <mkelwin@outlook.com>
2022-07-06 11:16:25 -0300

    sistema de busca finalizado, utilizado REGEX

---
+++ js/busca.js
@@ -0,0 +1,24 @@
+var campoBusca = document.querySelector('#buscaAluno');
+
+campoBusca.addEventListener('input', () => {
+    var alunos = document.querySelectorAll('.paciente');
+    if(campoBusca.value.length > 0) {
+        for (i = 0; i < alunos.length; i++) {
+            var aluno = alunos[i];
+            var nome = (aluno.querySelector('.info-nome')).textContent;
+            var regex = new RegExp(campoBusca.value, "i");
+
+            if (regex.test(nome)) {
+                aluno.classList.remove("invisivel");
+            } else {
+                aluno.classList.add("invisivel");
+            }
+
+        }   
+    } else if (campoBusca.value.length == 0) {
+        for (i = 0; i < alunos.length; i++) {
+            alunos[i].classList.remove('invisivel');
+        }
+    }
+    
+});
