1c04d7184409ef85564fbf8a04a4994238d878df
Kelwin Hollunder <mkelwin@outlook.com>
2023-03-16 12:45:26 -0300

    Update importarComAjax.js

--- js/importarComAjax.js
+++ js/importarComAjax.js
@@ -1,21 +1,21 @@
-var botaoBusca = document.querySelector('#busca');
+// var botaoBusca = document.querySelector('#busca');
 
-botaoBusca.addEventListener("click", function() {
-    console.log("buscando alunos");
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
-    xhr.send();
+// botaoBusca.addEventListener("click", function() {
+//     console.log("buscando alunos");
+//     var xhr = new XMLHttpRequest();
+//     xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
+//     xhr.send();
 
-    xhr.addEventListener("load", () => {
-        if (xhr.status == 200) {
-            let resposta = xhr.responseText;
-            let alunos = JSON.parse(resposta);
+//     xhr.addEventListener("load", () => {
+//         if (xhr.status == 200) {
+//             let resposta = xhr.responseText;
+//             let alunos = JSON.parse(resposta);
             
-            alunos.forEach(aluno => {         
-                geraHTML(aluno.nome, aluno.peso, aluno.altura, aluno.gordura);   
-                avaliaIMC(alunoGerado);                
-            });
-            atualizaView(alunos.length);
-        } else alert("O servidor não foi encontrado, tente novamente mais tarde");
-    });
-});
+//             alunos.forEach(aluno => {         
+//                 geraHTML(aluno.nome, aluno.peso, aluno.altura, aluno.gordura);   
+//                 avaliaIMC(alunoGerado);                
+//             });
+//             atualizaView(alunos.length);
+//         } else alert("O servidor não foi encontrado, tente novamente mais tarde");
+//     });
+// });
